9aa1b5201d0b1c9f26d97a9031d7057a8de215cc
Thomas Schuehly <thomas.schuehly@outlook.com>
2021-09-29 11:34:09 +0200

    @feature: Integration Test working, refactored application.yaml, updated gradlew

--- datarecovery-backend/src/test/kotlin/de/tschuehly/datarecoverybackend/controller/OrderControllerIntegrationTest.kt
+++ datarecovery-backend/src/test/kotlin/de/tschuehly/datarecoverybackend/controller/OrderControllerIntegrationTest.kt
@@ -2,22 +2,35 @@ package de.tschuehly.datarecoverybackend.controller
 
 import de.tschuehly.datarecoverybackend.util.IntegrationTestBase
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
-@Sql("INIT.sql")
+@Sql("classpath:database/init.sql")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(
+    locations = ["classpath:testEnvironment.env"]
+)
+@AutoConfigureMockMvc
+@ActiveProfiles(profiles = ["integrationTest"])
 internal class OrderControllerIntegrationTest() : IntegrationTestBase() {
 
+    @Autowired
+    lateinit var mockMvc: MockMvc
     @Test
     fun getArchived() {
     }
 
     @Test
     fun shouldReturnAllActiveOrders() {
-
+        mockMvc.get("/api/order").andExpect { status { 200 } }
     }
 
     @Test
