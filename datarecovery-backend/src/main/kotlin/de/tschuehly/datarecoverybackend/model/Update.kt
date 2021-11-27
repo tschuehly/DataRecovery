01688826c6067df37fac76338dae8ad4dfb532af
Thomas Schuehly <thomas.schuehly@outlook.com>
2021-11-27 17:36:08 +0100

    @fix: Pictures can now be up to 10mb, changed Update.description to CLOB instead of varchar

--- datarecovery-backend/src/main/kotlin/de/tschuehly/datarecoverybackend/model/Update.kt
+++ datarecovery-backend/src/main/kotlin/de/tschuehly/datarecoverybackend/model/Update.kt
@@ -1,13 +1,17 @@
 package de.tschuehly.datarecoverybackend.model
 
 import com.fasterxml.jackson.annotation.JsonBackReference
+import org.hibernate.annotations.Type
 import java.util.*
 import javax.persistence.*
 import kotlin.collections.ArrayList
 
 @Entity
 class Update(
+    @Column(length = 1000)
     var title: String?,
+    @Lob
+    @Type(type = "org.hibernate.type.StringType")
     var description: String? = null,
     var date: Date? = Date(),
     @OneToMany(cascade = [CascadeType.ALL])
