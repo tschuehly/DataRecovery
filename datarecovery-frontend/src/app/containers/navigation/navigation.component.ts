90f2e352c4c75e5ed59b50e0b644e15ca0ddc1a4
TobiasJungbauer <86930081+TobiasJungbauer@users.noreply.github.com>
2021-12-24 05:18:07 +0100

    Tobias changes (#95)
    * Diverse Änderungen

--- datarecovery-frontend/src/app/containers/navigation/navigation.component.ts
+++ datarecovery-frontend/src/app/containers/navigation/navigation.component.ts
@@ -28,10 +28,10 @@ declare let gtag: Function;
 
         <a class="cursor-pointer p-2" (click)="mobileNavShow = false" routerLink="">Startseite</a>
           <a class="cursor-pointer p-2" (click)="scrollToOrder(); mobileNavShow = false">Auftragsformular</a>
-          <a class="cursor-pointer p-2" (click)="mobileNavShow = false" routerLink="preise">Preise</a>
+          <a class="cursor-pointer p-2" (click)="mobileNavShow = false" routerLink="preise">Preisliste</a>
           <div class="h-full text-center flex"  (clickOutside)="dropdownShow = false" [exclude]="'div.dropdown'"
                [excludeBeforeClick]="true" [ngClass]="{'bg-gray-300 md:bg-black ': dropdownShow}">
-            <a class="cursor-pointer self-center p-2" (click)="dropdownShow = !dropdownShow">Datenrettung</a>
+            <a class="cursor-pointer self-center p-2" (click)="dropdownShow = !dropdownShow">Arbeitsweise</a>
             
 
 
