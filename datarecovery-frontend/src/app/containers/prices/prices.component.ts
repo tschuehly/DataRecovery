57c313c8f9f0042a9cdb895f9267aac130fd2c16
Tobias Jungbauer <86930081+TobiasJungbauer@users.noreply.github.com>
2022-02-18 04:45:14 +0100

    Preise Textänderungen + Button

--- datarecovery-frontend/src/app/containers/prices/prices.component.ts
+++ datarecovery-frontend/src/app/containers/prices/prices.component.ts
@@ -23,11 +23,7 @@ import {Router} from '@angular/router';
           <span class="text-white font-semibold">Festpreise</span>, die <span class=" text-white">nur nach Erfolg</span> gelten.<br>
           <span class="text-white font-semibold">Bei Misserfolg</span> erhalten Sie Ihren Speicher <span class="text-white font-semibold">kostenfrei </span>zurück.<br>
           Alle angegebenen Preise sind bereits inklusive 19% Mehrwertsteuer.<br>
-          <div class="flex justify-center pt-8">
-          <button class="text-xl bg-white py-3 px-4 shadow rounded text-black" (click)="scrollToOrder();">
-              weiter zum Ablauf & Auftrag
-            </button> 
-            </div>
+        
         </div>
 
       </div>
@@ -35,7 +31,7 @@ import {Router} from '@angular/router';
     <div class="grid grid-cols-1 xl:grid-cols-2  border-b-2 border-t-2">
       <div class="text-white">
           <div class="bg-gray-main p-12" style="box-shadow: 0 2px 25px 0 black">
-          
+          <span class="font-bold text-2xl underline text-white">Festpreise:</span>
             <ng-container *ngFor="let category of categories " >
 
             <div class="mb-2">
@@ -47,8 +43,13 @@ import {Router} from '@angular/router';
                   </div>
                 </ng-container>
               </div>
+              
             </ng-container>
-            
+            <div class="flex justify-end pt-8">
+          <button class="text-xl bg-white py-3 px-4 shadow rounded text-black" (click)="scrollToOrder();">
+              weiter zum Ablauf & Auftrag
+            </button> 
+            </div>
         </div>
       </div>
 
