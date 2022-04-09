a1f3d7aebed4093ffbeafe8f3ac22132507b7e01
Tobias Jungbauer <86930081+TobiasJungbauer@users.noreply.github.com>
2022-04-09 11:04:54 +0200

    font ...

--- datarecovery-frontend/src/app/components/review-component/review-component.component.ts
+++ datarecovery-frontend/src/app/components/review-component/review-component.component.ts
@@ -14,7 +14,7 @@ SwiperCore.use([Virtual, Navigation, A11y, Pagination, EffectFlip, EffectCube]);
         <div>
           <h3 class="text-3xl md:text-4xl font-semibold mb-4 ">Bewertungen aus Google</h3>
 
-          <div class="flex justify-center  text-xl md:text-2xl pt-4 items-center">
+          <div class="flex justify-center pt-4 items-center">
             <svg class=" text-yellow-400 mx-1 w-8 h-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
               <path
               d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/>
@@ -42,7 +42,7 @@ SwiperCore.use([Virtual, Navigation, A11y, Pagination, EffectFlip, EffectCube]);
        </span>
         </div>
       </div>
-      <div class="px-0 md:container  text-lg md:text-xl py-4">
+      <div class="px-0 md:container py-4">
         <swiper class="my-4"
                 [config]="config">
           <ng-template swiperSlide *ngFor="let currentReview of reviews">
