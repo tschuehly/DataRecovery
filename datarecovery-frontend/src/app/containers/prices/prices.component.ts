d3ab425f8812b15104ff48edc6ee08f362ac05be
Tobias Jungbauer <86930081+TobiasJungbauer@users.noreply.github.com>
2022-04-10 15:24:37 +0200

    prrces font size

--- datarecovery-frontend/src/app/containers/prices/prices.component.ts
+++ datarecovery-frontend/src/app/containers/prices/prices.component.ts
@@ -44,12 +44,12 @@ import { Router } from '@angular/router';
           class="bg-gray-main px-4 py-12"
           style="box-shadow: 0 2px 25px 0 black"
         >
-          <span class="font-bold text-lg md:text-2xl underline text-white"
+          <span class="font-bold text-2xl underline text-white"
             >Festpreise nach Erfolg:</span
           >
           <ng-container *ngFor="let category of categories">
             <div class="mb-2">
-              <h1 class="font-semibold text-lg md:text-2xl mt-4 pb-2">
+              <h1 class="font-semibold text-base md:text-2xl mt-4 pb-2">
                 {{ category.title }}
               </h1>
               <div class="whitespace-pre-wrap" *ngIf="category.description">
