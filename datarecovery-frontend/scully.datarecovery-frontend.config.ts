5ca07c65ce3bb6be74a92f64818a422b886b1815
Thomas Schuehly <thomas.schuehly@outlook.com>
2022-04-18 23:00:25 +0200

    chore: fix nginx.conf

--- datarecovery-frontend/scully.datarecovery-frontend.config.ts
+++ datarecovery-frontend/scully.datarecovery-frontend.config.ts
@@ -1,5 +1,13 @@
-import { HandledRoute, registerPlugin, ScullyConfig } from '@scullyio/scully';
+import {
+  ContentTextRoute,
+  HandledRoute,
+  registerPlugin,
+  scullyConfig,
+  ScullyConfig,
+} from '@scullyio/scully';
 import '@scullyio/scully-plugin-puppeteer';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 import { myPlugin } from './scully/plugins';
 export const config: ScullyConfig = {
   projectRoot: './src',
@@ -8,20 +16,47 @@ export const config: ScullyConfig = {
   outDir: './dist/static', // directory for scully build artifacts
   defaultPostRenderers: [myPlugin],
   routes: {
-    '/blog': {
+    /*
+    '/api/article/:articleUrl': {
+      type: 'contentFolder',
+      articleUrl: {
+        folder: '../datarecovery-backend/src/main/resources/templates/article',
+      },
+    },
+    '/api/article/datenrettung-kosten': {
+      type: 'default',
+      contentType: 'html',
+      content: async () => {
+        return readFileSync(
+          '../datarecovery-backend/src/main/resources/templates/article/datenrettung-kosten.html'
+        ).toString('utf-8');
+      },
+    },*/
+
+    '/tracking/keyword': {
       type: 'ignored',
     },
-    '/blog/:articleUrl': {
-      type: 'ignored',
-    },
-    '/tracking/:trackingId/:postalCode': {
+    '/tracking/category': {
       type: 'ignored',
     },
     '/tracking': {
+      type: 'ignored',
+    },
+    '/tracking/product': {
+      type: 'ignored',
+    },
+    '/tracking/login': {
+      type: 'ignored',
+    },
+    '/tracking/order': {
+      type: 'ignored',
+    },
+    '/datenrettung': {
       type: 'ignored',
     },
   },
   puppeteerLaunchOptions: {
     args: ['--no-sandbox', '--disable-setuid--sandbox'],
   },
+  extraRoutes: [],
 };
