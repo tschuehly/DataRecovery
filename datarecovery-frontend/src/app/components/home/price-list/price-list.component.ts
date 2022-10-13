a7a043661e8e0ee28d2e94752d7f2d380f478bd6
Tobias Jungbauer <86930081+TobiasJungbauer@users.noreply.github.com>
2022-10-13 23:11:44 +0200

    Text

--- datarecovery-frontend/src/app/components/home/price-list/price-list.component.ts
+++ datarecovery-frontend/src/app/components/home/price-list/price-list.component.ts
@@ -43,19 +43,19 @@ import { Component } from '@angular/core';
                 Defekte(r) USB Stick / (micro-) SD Karte
               </h2>
               <div class="p-4 leading-relaxed border-r border-white text-lg md:text-xl">
-              <div class="pb-2">Festpreise¹</div>
-                <div>99,00 €</div>
-                <div>150,00 €</div>
-                <div>249,00 €</div>
-                <div>449,00 €</div>
-              </div>
-              <div class="p-4 leading-relaxed border-l border-white text-lg md:text-xl">
-                <div class="pb-2">Speichergrößen</div>
+              <div class="pb-2">Speichergröße</div>
                 <div>≤ 32 GB</div>
                 <div>≤ 64 GB</div>
                 <div>≤ 128 GB</div>
                 <div>≤ 256 GB</div>
               </div>
+              <div class="p-4 leading-relaxed border-l border-white text-lg md:text-xl">
+              <div class="pb-2">Festpreis¹</div>
+              <div>99,00 €</div>
+              <div>150,00 €</div>
+              <div>249,00 €</div>
+              <div>449,00 €</div>
+            </div>
             </div>
             <div class="flex-1 rounded-tr-2xl">
               <picture>
@@ -91,18 +91,18 @@ import { Component } from '@angular/core';
                 Beschädigte HDD-Festplatte
               </h2>
               <div class="p-4 leading-relaxed border-r border-white text-lg md:text-xl">
-              <div class="pb-2">Festpreise¹ </div>
-                <div>299,00 €</div>
-                <div>349,00 €</div>
-                <div>449,00 €</div>
-                <div>549,00 €</div>
-              </div>
-              <div class="p-4 leading-relaxed border-l border-white text-lg md:text-xl">
-              <div class="pb-2">Speichergrößen</div>
+              <div class="pb-2">Speichergröße</div>
                 <div>≤ 1 TB</div>
                 <div>≤ 2 TB</div>
                 <div>≤ 4 TB</div>
                 <div>≤ 6 TB</div>
+              </div>
+              <div class="p-4 leading-relaxed border-l border-white text-lg md:text-xl">
+              <div class="pb-2">Festpreis¹ </div>
+                <div>299,00 €</div>
+                <div>349,00 €</div>
+                <div>449,00 €</div>
+                <div>549,00 €</div>
               </div>
             </div>
             <div class="grid grid-cols-2 py-4">
@@ -110,18 +110,18 @@ import { Component } from '@angular/core';
                 Defekter SSD-Speicher (SATA-basiert)
               </h2>
               <div class="p-4 leading-relaxed border-r border-white text-lg md:text-xl">
-              <div class="pb-2">Festpreise¹</div>
-                <div>199,00 €</div>
-                <div>249,00 €</div>
-                <div>299,00 €</div>
-                <div>399,00 €</div>
+              <div class="pb-2">Speichergröße</div>
+              <div>≤ 512 GB</div>
+              <div>≤ 1 TB</div>
+              <div>≤ 2 TB</div>
+              <div>≤ 4 TB</div>
               </div>
               <div class="p-4 leading-relaxed border-l border-white text-lg md:text-xl">
-              <div class="pb-2">Speichergrößen</div>
-                <div>≤ 512 GB</div>
-                <div>≤ 1 TB</div>
-                <div>≤ 2 TB</div>
-                <div>≤ 4 TB</div>
+              <div class="pb-2">Festpreis¹</div>
+              <div>199,00 €</div>
+              <div>249,00 €</div>
+              <div>299,00 €</div>
+              <div>399,00 €</div>
               </div>
             </div>
 
