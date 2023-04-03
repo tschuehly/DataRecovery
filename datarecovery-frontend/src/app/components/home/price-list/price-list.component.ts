bc69c7a2ec30e0a232b17c435218502104cb29ff
Tobias Jungbauer <86930081+TobiasJungbauer@users.noreply.github.com>
2023-04-03 18:29:04 +0200

    Update price-list.component.ts

--- datarecovery-frontend/src/app/components/home/price-list/price-list.component.ts
+++ datarecovery-frontend/src/app/components/home/price-list/price-list.component.ts
@@ -50,7 +50,7 @@ import { Component } from '@angular/core';
               </div>
               <div class="p-4 leading-relaxed border-l border-white text-lg md:text-xl">
               <div class="pb-2">Festpreis¹</div>
-              <div>119,00 €</div>
+              <div>99,00 €</div>
               <div>149,00 €</div>
               <div>249,00 €</div>
             </div>
