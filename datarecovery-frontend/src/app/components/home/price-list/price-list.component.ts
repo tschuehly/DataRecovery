dd33ed9b4e00229fb353e8162483a6a26cec885b
Tobias Jungbauer <86930081+TobiasJungbauer@users.noreply.github.com>
2023-04-11 04:23:59 +0200

    text

--- datarecovery-frontend/src/app/components/home/price-list/price-list.component.ts
+++ datarecovery-frontend/src/app/components/home/price-list/price-list.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
       <h2
         class="pt-10 py-4 text-2xl md:text-3xl tracking-wider text-center text-gray-50"
       >
-      <a class="underline" href="preise">Preise für "schwierige" Fälle</a>:
+      <a class="underline" href="preise">Kosten "schwierige" Fälle</a>:
       </h2>
 
       <div
@@ -185,7 +185,7 @@ import { Component } from '@angular/core';
               <button
                 class="px-4 py-2 text-lg md:text-xl text-white rounded shadow bg-gray-main"
               >
-                <a href="preise">-> Preise für "einfache" Fälle</a>
+                <a href="preise">Kosten "einfache" Fälle</a>
               </button>
             </td>
 
