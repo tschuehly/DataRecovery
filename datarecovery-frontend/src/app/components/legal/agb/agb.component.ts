8ae49b8cca8db2520de05321836992f382eca981
Tobias Jungbauer <86930081+TobiasJungbauer@users.noreply.github.com>
2023-04-30 11:41:31 +0200

    Update agb.component.ts

--- datarecovery-frontend/src/app/components/legal/agb/agb.component.ts
+++ datarecovery-frontend/src/app/components/legal/agb/agb.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 
         <p>für Dienstleistungen der Firma Tobias Jungbauer Datenrettung</p>
 
-        <p><span style="font-style:italic">Stand: 2020/21</span></p>
+        <p><span style="font-style:italic">Stand: 2023</span></p>
         <br />
         Adresse:<br />
         <p>
@@ -35,376 +35,104 @@ import { Component, OnInit } from '@angular/core';
 
         <p><span class="wixGuard">​</span></p>
 
-        <p><span style="text-decoration:underline">1. Geltungsbereich</span></p>
+        <p><span style="text-decoration:underline">1. Vertragsgegenstand</span></p>
 
         <p>
-          Diese Geschäftsbedingungen gelten für alle Kaufverträge und
-          Werkverträge bei Tobias Jungbauer Datenrettung, die von Privatkunden
-          oder von Geschäftskunden getätigt werden. Zusätzlich zu den AGB gelten
-          §§ 631 ff. BGB.
-        </p>
-
-        <p>
-          Privatkunden in diesem Sinne sind Personen mit Wohnsitz und
-          Lieferadresse in der Bundesrepublik Deutschland, soweit die von ihnen
-          bestellten Waren weder ihrer gewerblichen noch ihrer selbstständigen
-          beruflichen Tätigkeit zugerechnet werden können.
+        Der Auftragnehmer verpflichtet sich, im Rahmen eines Werksvertrags (§§ 631 ff. BGB) zur Datenrettung, 
+        die auf dem Datenträger des Auftraggebers gespeicherten Daten wiederherzustellen. 
+        Der Auftragnehmer übernimmt dabei die Verantwortung für den sachgemäßen Umgang mit den Daten 
+        und die Einhaltung aller geltenden Datenschutzbestimmungen.
         </p>
 
         <p>&nbsp;</p>
 
         <p>
           <span style="text-decoration:underline"
-            >2. Preise und Versandkosten</span
+            >2. Preise</span
           >
         </p>
 
         <p>
-          Die ausgezeichneten Preise sind Endpreise inkl. 19% Umsatzsteuer. Es
-          gilt der Betrag, der jeweils zum Zeitpunkt des verbindlichen Auftrags
-          ausgewiesen ist. Die Kosten der Rücksendung, die im Falle einer
-          Rückgabe ohne Datenrettungserfolg entstehen, tragen wir.
+        Die Kosten für die Datenrettung werden ausschließlich auf der Grundlage des Erfolgs berechnet. 
+        Dies bedeutet, dass der Kunde erst nach erfolgreicher Datenrettung zahlen muss. 
+        Erfolgreiche Datenrettung wird als Wiederherstellung von mindestens 95% der verlorenen Daten definiert 
+        oder durch die schriftliche Bestätigung des Kunden, dass die Datenrettung erfolgreich war.
         </p>
 
         <p>&nbsp;</p>
 
-        <p><span style="text-decoration:underline">3. Zahlung</span></p>
+        <p><span style="text-decoration:underline">3. Teildatenrettung</span></p>
 
-        <p>Die Bezahlung erfolgt bei Lieferung mittels</p>
+        <p>Sollte eine vollständige Datenrettung nicht möglich sein, wird dem Kunden eine Teildatenrettung angeboten. 
+        Der Kunde wird die Möglichkeit haben, die geretteten Daten einzusehen, um zu entscheiden, 
+        ob er die Daten akzeptieren möchte oder nicht. Die Einsicht erfolgt mittels Teamviewer.</p>
 
-        <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; - Rechnung</p>
 
         <p>&nbsp;</p>
 
-        <p><span style="text-decoration:underline">4. Zahlungsverzug</span></p>
+        <p><span style="text-decoration:underline">4. Verantwortung des Kunden</span></p>
 
         <p>
-          Kommen Sie in Zahlungsverzug, so ist Tobias Jungbauer Datenrettung
-          berechtigt, Verzugszinsen in Höhe von 5 Prozentpunkten über dem von
-          der Deutschen Bundesbank für den Zeitpunkt der Bestellung bekannt
-          gegebenen Basiszinssatz p.a. zu fordern. Falls Tobias Jungbauer
-          Datenrettung ein höherer Verzugsschaden nachweisbar entstanden ist,
-          ist Tobias Jungbauer Datenrettung berechtigt, diesen geltend zu
-          machen.
+        Der Kunde ist dafür verantwortlich, den Auftragnehmer über alle erforderlichen Informationen und Daten zu informieren, 
+        um eine erfolgreiche Datenrettung zu ermöglichen. Der Kunde muss auch sicherstellen, dass er über das Recht verfügt, 
+        die betreffenden Daten wiederherzustellen und dass er über alle erforderlichen Rechte und Lizenzen verfügt, 
+        um auf die Daten zuzugreifen.
         </p>
 
         <p>&nbsp;</p>
 
         <p>
           <span style="text-decoration:underline"
-            >5. Zurückbehaltungsrecht</span
+            >5. Haftung</span
           >
         </p>
 
         <p>
-          Zur Ausübung eines Zurückbehaltungsrechts ist der Kunde nur insoweit
-          befugt, als sein Gegenanspruch auf demselben Vertragsverhältnis
-          beruht.
+        Der Auftragnehmer haftet für alle Schäden, die auf grobe Fahrlässigkeit oder Vorsatz zurückzuführen sind. 
+        Für leicht fahrlässig verursachte Schäden haftet der Auftragnehmer nur bei Verletzung wesentlicher Vertragspflichten 
+        oder bei Schäden aus der Verletzung des Lebens, des Körpers oder der Gesundheit.
         </p>
 
         <p>&nbsp;</p>
 
-        <p><span style="text-decoration:underline">6. Lieferung</span></p>
+        <p><span style="text-decoration:underline">6. Vertraulichkeit</span></p>
 
         <p>
-          (1) Die Lieferung erfolgt an die vom Kunden angegebene
-          Lieferanschrift, innerhalb von
-        </p>
-
-        <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; - Deutschland</p>
-
-        <p>
-          <br />
-          (2) Macht höhere Gewalt (Naturkatastrophen, Krieg, Bürgerkrieg,
-          Terroranschlag) die Lieferung oder eine sonstige Leistung dauerhaft
-          unmöglich, ist eine Leistungspflicht von Tobias Jungbauer Datenrettung
-          ausgeschlossen. Bereits gezahlte Beträge werden von Tobias Jungbauer
-          Datenrettung unverzüglich erstattet.
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          (3) Tobias Jungbauer Datenrettung kann außerdem die Leistung
-          verweigern, soweit diese einen Aufwand erfordert, der unter Beachtung
-          des Inhalts und der Gebote von Treu und Glauben in einem groben
-          Missverhältnis zu dem Interesse des Kunden an der Erfüllung des
-          Vertrages steht. Bereits gezahlte Beträge werden von Tobias Jungbauer
-          Datenrettung unverzüglich erstattet.
+        Der Auftragnehmer behandelt alle dem Unternehmen zur Verfügung gestellten Informationen und Daten des Kunden vertraulich 
+        und gewährleistet, dass keine Daten an Dritte weitergegeben werden.
         </p>
 
         <p>&nbsp;</p>
 
         <p>
           <span style="text-decoration:underline"
-            >7. Günstige Versandart bei Rücksendung</span
+            >7. Gerichtsstand und anwendbares Recht</span
           >
         </p>
 
         <p>
-          (1) Bitte verwenden Sie bei der Rücksendung der Ware und des Zubehörs
-          nach Möglichkeit die Originalverpackung, auch wenn diese durch eine
-          Öffnung zur Funktionsprüfung beschädigt sein sollte.
+        Für alle Streitigkeiten im Zusammenhang mit diesem Vertrag gilt das Recht der Bundesrepublik Deutschland. 
         </p>
 
         <p>&nbsp;</p>
 
+        <p><span style="text-decoration:underline">8. Änderungen und Ergänzungen des Vertrags</span></p>
+
         <p>
-          (2) Bitte verwenden Sie zur Rücksendung den der Warenlieferung
-          beigefügten, vollständig frankierten und adressierten
-          Rücksendeaufkleber. Es handelt sich dabei um die einfachste und
-          kostengünstigste Versandmöglichkeit. Sie trifft keine Verpflichtung
-          zur Verwendung dieses Rücksendeverfahrens. Wenn Sie allerdings eine
-          unnötig teure Versandart wählen, können Sie unter Umständen
-          verpflichtet sein, die gegenüber einer günstigen Versandart erhöhten
-          Kosten an uns zu zahlen.
+        Änderungen und Ergänzungen dieses Vertrags bedürfen der Schriftform. Dies gilt auch für die Aufhebung dieser Schriftformklausel. 
+        Mündliche Nebenabreden bestehen nicht.
+        </p>
+
+        <p><span style="text-decoration:underline">9. Salvatorische Klausel</span></p>
+
+        <p>
+        Sollten einzelne Bestimmungen dieses Vertrags unwirksam oder undurchführbar sein oder werden, 
+        so wird dadurch die Wirksamkeit der übrigen Bestimmungen nicht berührt.
         </p>
 
         <p>&nbsp;</p>
 
-        <p><span style="text-decoration:underline">8. Mängelrechte</span></p>
-
-        <p>
-          (1) Ein bereits bei der Lieferung mangelhaftes Produkt
-          (Gewährleistungsfall) wird Tobias Jungbauer Datenrettung nach Wahl des
-          Kunden auf Kosten von Tobias Jungbauer Datenrettung durch ein
-          mangelfreies ersetzen oder fachgerecht reparieren lassen
-          (Nacherfüllung). Der Kunde wird darauf hingewiesen, dass kein
-          Gewährleistungsfall vorliegt, wenn das Produkt bei Gefahrübergang die
-          vereinbarte Beschaffenheit hatte. Ein Gewährleistungsfall liegt
-          insbesondere in folgenden Fällen nicht vor:
-        </p>
-
-        <p>
-          a)&nbsp; bei Schäden, die beim Kunden durch Missbrauch oder
-          unsachgemäßen Gebrauch entstanden sind,
-        </p>
-
-        <p>
-          b)&nbsp; bei Schäden, die dadurch entstanden sind, dass die Produkte
-          beim Kunden schädlichen äußeren Einflüssen ausgesetzt worden sind
-          (insbesondere extremen Temperaturen, Feuchtigkeit, außergewöhnlicher
-          physikalischer oder elektrischer Beanspruchung, Spannungsschwankungen,
-          Blitzschlag, statischer Elektrizität, Feuer).
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          (2) Tobias Jungbauer Datenrettung leistet ferner keine Gewähr für
-          einen Fehler, der durch unsachgemäße Reparatur durch einen nicht vom
-          Hersteller autorisierten Servicepartner entstanden ist.
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          (3) Erfordert die vom Kunden gewünschte Art der Nacherfüllung
-          (Ersatzlieferung oder Reparatur) einen Aufwand, der in Anbetracht des
-          Produktpreises unter Beachtung des Vertragsinhaltes und der Gebote von
-          Treu und Glauben in einem groben Missverhältnis zu dem
-          Leistungsinteresse des Kunden steht – wobei insbesondere der Wert des
-          zu verrichtenden Werkes im mangelfreien Zustand, die Bedeutung des
-          Mangels und die Frage zu berücksichtigen sind, ob auf die andere Art
-          der Nacherfüllung ohne erhebliche Nachteile für den Kunden
-          zurückgegriffen werden kann – beschränkt sich der Anspruch des Kunden
-          auf die jeweils andere Art der Nacherfüllung. Das Recht von Tobias
-          Jungbauer Datenrettung, auch diese andere Art der Nacherfüllung unter
-          der vorgenannten Voraussetzung zu verweigern, bleibt unberührt.&nbsp;
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          (4) Sowohl für den Fall der Reparatur als auch für den Fall der
-          Ersatzlieferung ist der Kunde verpflichtet, das Produkt auf Kosten von
-          Tobias Jungbauer Datenrettung unter Angabe der Auftragsnummer an die
-          von ihr angegebene Rücksendeadresse einzusenden.&nbsp;
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          (5) Sendet der Kunde die Ware ein, um ein Austauschprodukt zu
-          bekommen, richtet sich die Rückgewähr des mangelhaften Produktes nach
-          folgender Maßgabe: Sofern der Kunde die Ware zwischen Lieferung und
-          Rücksendung in mangelfreiem Zustand benutzen konnte, hat dieser den
-          Wert der von ihm gezogenen Nutzungen zu erstatten. Für einen nicht
-          durch den Mangel eingetretenen Untergang oder die weitere
-          Verschlechterung der Ware sowie für die nicht durch den Mangel
-          eingetretene Unmöglichkeit der Herausgabe der Ware im Zeitraum
-          zwischen Lieferung der Ware und Rücksendung der Ware hat der Kunde
-          Wertersatz zu leisten. Der Kunde hat keinen Wertersatz für die durch
-          den bestimmungsgemäßen Gebrauch der Ware entstandene Verschlechterung
-          der Ware zu leisten. Die Pflicht zum Wertersatz entfällt für die
-          Rücksendung eines mangelhaften Produktes im Gewährleistungsfall
-          ferner,
-        </p>
-
-        <p>
-          a)&nbsp; wenn sich der zum Rücktritt berechtigende Mangel erst während
-          der Verarbeitung oder Umgestaltung gezeigt hat,
-        </p>
-
-        <p>
-          b)&nbsp; wenn Tobias Jungbauer Datenrettung die Verschlechterung oder
-          den Untergang zu vertreten hat oder der Schaden auch bei Tobias
-          Jungbauer Datenrettung eingetreten wäre,
-        </p>
-
-        <p>
-          c)&nbsp; wenn die Verschlechterung oder der Untergang beim Kunden
-          eingetreten ist, obwohl dieser diejenige Sorgfalt beachtet hat, die er
-          in eigenen Angelegenheiten anzuwenden pflegt.
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          (6) Die Schadensersatzpflicht des Kunden bei einer vom Kunden zu
-          vertretenden Verletzung der Rücksendungspflicht richtet sich nach
-          Maßgabe der gesetzlichen Bestimmungen.
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          (7) Der Kunde kann nach seiner Wahl vom Vertrag zurücktreten oder den
-          Kaufpreis mindern, wenn die Reparatur oder Ersatzlieferung innerhalb
-          einer angemessenen Frist nicht zu einem vertragsgerechten Zustand des
-          Produktes geführt hat.&nbsp;
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          (8) Darüber hinaus können auch Ansprüche gegen den Hersteller im
-          Rahmen einer von diesem eingeräumten Garantie bestehen, die sich nach
-          den entsprechenden Garantiebedingungen richten.&nbsp;
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          9) Die gesetzliche Gewährleistung von Tobias Jungbauer Datenrettung
-          endet zwei Jahre ab Lieferung der Ware. Die Frist beginnt mit dem
-          Erhalt der Ware.
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p><span style="text-decoration:underline">9. Haftung</span></p>
-
-        <p>
-          (1) Bei leichter Fahrlässigkeit haftet Tobias Jungbauer Datenrettung
-          nur bei der Verletzung vertragswesentlicher Pflichten, deren Erfüllung
-          zur Erreichung des Vertragszwecks notwendig sind, und beschränkt auf
-          den vorhersehbaren Schaden. Diese Beschränkung gilt nicht bei der
-          Verletzung von Leben, Körper und Gesundheit. Für sonstige leicht
-          fahrlässig durch einen Mangel des zu verrichtenden Werkes verursachte
-          Schäden haftet Tobias Jungbauer Datenrettung nicht.
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          (2) Unabhängig von einem Verschulden von Tobias Jungbauer Datenrettung
-          bleibt eine Haftung von Tobias Jungbauer Datenrettung bei arglistigem
-          Verschweigen des Mangels oder aus der Übernahme einer Garantie
-          unberührt. Die Herstellergarantie ist eine Garantie des Herstellers
-          und stellt keine Übernahme einer Garantie durch Tobias Jungbauer
-          Datenrettung dar.
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          (3) Tobias Jungbauer Datenrettung ist auch für die während ihres
-          Verzugs durch Zufall eintretende Unmöglichkeit der Lieferung
-          verantwortlich, es sei denn, dass der Schaden auch bei rechtzeitiger
-          Lieferung eingetreten wäre.
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          (4) Ausgeschlossen ist die persönliche Haftung der gesetzlichen
-          Vertreter, Erfüllungsgehilfen und Betriebsangehörigen von Tobias
-          Jungbauer Datenrettung für von ihnen durch leichte Fahrlässigkeit
-          verursachte Schäden.
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          <span style="text-decoration:underline">10. Anwendbares Recht</span>
-        </p>
-
-        <p>
-          Der zwischen Ihnen und Tobias Jungbauer Datenrettung abgeschlossene
-          Vertrag unterliegt ausschließlich dem Recht der Bundesrepublik
-          Deutschland unter ausdrücklichem Ausschluss des UN-Kaufrechts.
-          Unberührt davon bleiben die zwingenden Bestimmungen des Staates, in
-          dem Sie Ihren gewöhnlichen Aufenthalt haben.
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p><span style="text-decoration:underline">11. Gerichtsstand</span></p>
-
-        <p>
-          Sofern Sie entgegen Ihren Angaben bei der Bestellung keinen Wohnsitz
-          in der Bundesrepublik Deutschland haben oder nach Vertragsabschluss
-          Ihren Wohnsitz ins Ausland verlegen oder Ihr Wohnsitz zum Zeitpunkt
-          der Klageerhebung nicht bekannt ist, ist Gerichtsstand für alle
-          Streitigkeiten aus und im Zusamhttps:// mit dem Vertragsverhältnis
-          [Sitz des Betreibers des https://ine-Shops].
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          <span style="text-decoration:underline">12. Streitbeilegung</span>
-        </p>
-
-        <p>
-          Allgemeine Informationspflichten zur alternativen Streitbeilegung nach
-          Art. 14 Abs. 1 ODR-VO und § 36 VSBG
-          (Verbraucherstreitbeilegungsgesetz):
-        </p>
-
-        <p>
-          Die europäische Kommission stellt eine Plattform zur
-          Online-Streitbelegung (OS) zur Verfügung, die Sie unter dieser Adresse
-          finden:
-          <span style="text-decoration:underline"
-            ><a
-              href="https://ec.europa.eu/consumers/odr/"
-              target="_blank"
-              rel="noopener"
-              >https:///ec.europa.eu/consumers/odr/</a
-            ></span
-          >
-          . Zur Teilnahme an einem Streitbeilegungsverfahren vor einer
-          Verbraucherschlichtungsstelle sind wir nicht verpflichtet und auch
-          nicht bereit.
-        </p>
-
-        <p>Schlussbestimmungen</p>
-
-        <p>
-          (1) Sollten einzelne Bestimmungen dieses Vertrages ganz oder teilweise
-          unwirksam oder nichtig sein oder werden, so wird dadurch die
-          Wirksamkeit des Vertrages im Übrigen nicht berührt, insoweit ein
-          Vertragspartner hierdurch nicht unangemessen benachteiligt wird.
-        </p>
-
-        <p>&nbsp;</p>
-
-        <p>
-          (2) Änderungen oder Ergänzungen dieses Vertrages bedürfen der
-          Schriftform.
-        </p>
+        
 
         <p><span class="wixGuard">​</span></p>
 
