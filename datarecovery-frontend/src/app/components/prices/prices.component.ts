c60f7fdedac42439560d5d11523c5d08715e332e
Tobias Jungbauer <86930081+TobiasJungbauer@users.noreply.github.com>
2023-05-30 06:42:56 +0200

    text

--- datarecovery-frontend/src/app/components/prices/prices.component.ts
+++ datarecovery-frontend/src/app/components/prices/prices.component.ts
@@ -73,7 +73,7 @@ import { ScrollService } from 'src/app/services/scroll.service';
             </button>
           </div>
           <br />Alle Preise sind bereits inklusive 19% Mehrwertsteuer und nur gelten nach Erfolg.<br/>
-          <br />Bei Misserfolg ist die Bearbeitung inkl. Rückversand zu 100% kostenfrei.<br/>
+          <br />Bei Misserfolg ist die Bearbeitung inkl. Rückversand des Originalspeichers kostenfrei.<br/>
         </div>
       </div>
 
