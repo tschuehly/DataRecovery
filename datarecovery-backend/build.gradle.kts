c161df98b8b30bdf3fed582fe4b80559afe1b5f2
Thomas Schuehly <thomas.schuehly@mimacom.com>
2023-07-13 22:13:48 +0200

    upgrade spring dependencies

--- datarecovery-backend/build.gradle.kts
+++ datarecovery-backend/build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.3"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.0"
-    kotlin("plugin.spring") version "1.5.0"
-    kotlin("plugin.jpa") version "1.5.0"
+    id("org.springframework.boot") version "2.7.13"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "de.tschuehly"
@@ -23,20 +23,20 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mustache")
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.liquibase:liquibase-core:4.4.3")
+    implementation("org.liquibase:liquibase-core:4.20.0")
     implementation("org.postgresql:postgresql")
-    implementation("com.auth0:java-jwt:3.13.0")
+    implementation("com.auth0:java-jwt:4.3.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.github.paulcwarren:content-jpa-spring-boot-starter:1.2.5")
+    implementation("com.github.paulcwarren:content-jpa-spring-boot-starter:2.8.0")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("com.h2database:h2")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.testcontainers:testcontainers:1.16.0")
-    testImplementation("org.testcontainers:junit-jupiter:1.16.0")
-    testImplementation("org.testcontainers:postgresql:1.16.0")
-    testImplementation("com.icegreen:greenmail-junit5:1.6.5")
+    testImplementation("org.testcontainers:testcontainers:1.17.6")
+    testImplementation("org.testcontainers:junit-jupiter:1.17.6")
+    testImplementation("org.testcontainers:postgresql:1.17.6")
+    testImplementation("com.icegreen:greenmail-junit5:2.0.0")
     testImplementation("org.springframework.security:spring-security-test")
 
 }
