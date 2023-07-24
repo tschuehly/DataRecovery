ab24d53381f562df5046bddbb5de4ca1d1c7e3e4
Tobias Jungbauer <86930081+TobiasJungbauer@users.noreply.github.com>
2023-07-24 11:17:11 +0200

    Test

--- datarecovery-frontend/src/app/components/navigation/navigation.component.ts
+++ datarecovery-frontend/src/app/components/navigation/navigation.component.ts
@@ -144,7 +144,7 @@ declare let gtag: Function;
         <div class="flex justify-around items-center text-base md:text-lg pb-4">
           <span>
           <div class="flex justify-center">
-  <img class="h-28 md:h-32 pb-2" src="assets/Profil.jpg">
+  <img class="h-28 md:h-32 pb-2" src="assets/Profil.webp">
   </div>
             <span class="font-semibold pb-2">Haben Sie Fragen? </span>
             <p>Gerne können Sie diese per Telefon stellen:</p>
